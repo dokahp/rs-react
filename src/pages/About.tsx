307048775dafd9613011457171b7115ddc98ce54
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-03-19 21:17:30 +0300

    feat: simple change about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 class About extends React.PureComponent {
   render() {
-    return <h3 style={{ color: '#fff' }}>About</h3>;
+    return (
+      <div
+        style={{
+          color: '#fff',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h3>About Project</h3>
+        <p>This project would be a Beta version of youtube! &#128513; </p>
+      </div>
+    );
   }
 }
 
