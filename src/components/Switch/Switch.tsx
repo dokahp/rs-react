74137703da1504d635348c7c3464f671689f6a52
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-04-01 23:42:53 +0300

    refactor: change switch component to function component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -8,44 +8,35 @@ interface Props {
   handleSwitch: () => void;
 }
 
-class Switch extends React.PureComponent<Props, object> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { reference, name, isOn, handleSwitch } = this.props;
-
-    return (
+function Switch({ reference, name, isOn, handleSwitch }: Props) {
+  return (
+    <label
+      htmlFor="react-switch-new"
+      className="label"
+      style={{ display: 'flex', alignItems: 'center' }}
+    >
+      <input
+        ref={reference}
+        name={name}
+        onChange={handleSwitch}
+        className="react-switch-checkbox"
+        id="react-switch-new"
+        type="checkbox"
+      />
       <label
+        style={{ backgroundColor: isOn ? '#06D6A0' : '#808080' }}
+        className="react-switch-label"
         htmlFor="react-switch-new"
-        className="label"
-        style={{ display: 'flex', alignItems: 'center' }}
       >
-        <input
-          ref={reference}
-          name={name}
-          onChange={handleSwitch}
-          className="react-switch-checkbox"
-          id="react-switch-new"
-          type="checkbox"
-        />
-        <label
-          style={{ backgroundColor: isOn ? '#06D6A0' : '#808080' }}
-          className="react-switch-label"
-          htmlFor="react-switch-new"
-        >
-          <span className="react-switch-button" />
-        </label>
-        {isOn ? (
-          <div className="description">Yes, this video is for kids.</div>
-        ) : (
-          <div className="description">No, this video is not for children.</div>
-        )}
+        <span className="react-switch-button" />
       </label>
-    );
-  }
+      {isOn ? (
+        <div className="description">Yes, this video is for kids.</div>
+      ) : (
+        <div className="description">No, this video is not for children.</div>
+      )}
+    </label>
+  );
 }
 
 export default Switch;
