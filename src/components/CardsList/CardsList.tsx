3b90085b2d6b278c7d7060914f9e75a726dd8ddc
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-04-02 02:20:01 +0300

    refactor: change cardlist component to functional component

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -3,24 +3,15 @@ import Card from '../Card/Card';
 import './cardsList.css';
 import { Item, Items } from './interfaces/cardslist.interface';
 
-class CardsList extends React.Component<Items, Items> {
-  constructor(props: Items) {
-    super(props);
-    const { items } = this.props;
-    this.state = { items };
-  }
-
-  render() {
-    const { items } = this.state;
-    const cardsList = items.map((card: Item) => {
-      return <Card key={card.id.videoId} snippet={card.snippet} />;
-    });
-    return (
-      <div className="main-wrapper">
-        <div className="cards-wrapper">{cardsList}</div>
-      </div>
-    );
-  }
+function CardsList({ items }: Items) {
+  const cardsList = items.map((card: Item) => {
+    return <Card key={card.id.videoId} snippet={card.snippet} />;
+  });
+  return (
+    <div className="main-wrapper">
+      <div className="cards-wrapper">{cardsList}</div>
+    </div>
+  );
 }
 
 export default CardsList;
