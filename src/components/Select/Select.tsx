6e5687ff853792a137bbbb7c3510a9a96780c26a
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-04-02 00:07:47 +0300

    refactor: select component to functional component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -8,37 +8,27 @@ interface Props {
   msg: string;
 }
 
-class Select extends React.Component<Props, object> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { referance, name, err, msg } = this.props;
-    return (
-      <>
-        <select
-          className="select"
-          defaultValue="default"
-          name={name}
-          ref={referance}
-          style={
-            err
-              ? { marginBottom: '5px', borderBottom: '2px solid #d8000c' }
-              : {}
-          }
-        >
-          <option value="Video">Video</option>
-          <option value="Stream">Stream</option>
-          <option value="default" hidden>
-            Select video type*
-          </option>
-        </select>
-        {err ? <div className="error-block">{msg}</div> : ''}
-      </>
-    );
-  }
+function Select({ referance, name, err, msg }: Props) {
+  return (
+    <>
+      <select
+        className="select"
+        defaultValue="default"
+        name={name}
+        ref={referance}
+        style={
+          err ? { marginBottom: '5px', borderBottom: '2px solid #d8000c' } : {}
+        }
+      >
+        <option value="Video">Video</option>
+        <option value="Stream">Stream</option>
+        <option value="default" hidden>
+          Select video type*
+        </option>
+      </select>
+      {err ? <div className="error-block">{msg}</div> : ''}
+    </>
+  );
 }
 
 export default Select;
