403e9bcb9c41521e138642744ac68921ab88fa03
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-04-02 01:49:56 +0300

    refactor: change search component to functional component, change lifecycle loops to useEffect

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,53 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './search.css';
 import { ReactComponent as Loop } from './loop.svg';
 
-interface State {
-  searchValue: string;
-}
+function Search() {
+  const [searchValue, setSearchValue] = useState('');
 
-class Search extends React.Component<object, State> {
-  constructor(props: object) {
-    super(props);
-    this.state = { searchValue: '' };
-  }
-
-  componentDidMount(): void {
-    const searchValue = localStorage.getItem('search-value');
-    if (searchValue) {
-      this.setState({ searchValue });
+  useEffect(() => {
+    const localStorageValue = localStorage.getItem('search-value');
+    if (localStorageValue && localStorageValue.length > searchValue.length) {
+      setSearchValue(localStorageValue);
     }
-  }
+    return function cleanup() {
+      localStorage.setItem('search-value', searchValue);
+    };
+  }, [searchValue]);
 
-  componentWillUnmount(): void {
-    const { searchValue } = this.state;
-    localStorage.setItem('search-value', searchValue);
-  }
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchValue(e.currentTarget.value);
+  };
 
-  inputChange(e: React.ChangeEvent<HTMLInputElement>): void {
-    this.setState({ searchValue: e.currentTarget.value });
-  }
-
-  render() {
-    const { searchValue } = this.state;
-    return (
-      <div className="search-wrapper">
-        <input
-          className="search-input"
-          placeholder="Enter for Search"
-          onChange={this.inputChange.bind(this)}
-          value={searchValue}
+  return (
+    <div className="search-wrapper">
+      <input
+        className="search-input"
+        placeholder="Enter for Search"
+        onChange={inputChange}
+        value={searchValue}
+      />
+      <button className="search-button" type="submit">
+        <Loop
+          className="loop"
+          fill="white"
+          style={{ width: '24px', height: '24px', color: 'f1f1f1' }}
         />
-        <button className="search-button" type="submit">
-          <Loop
-            className="loop"
-            fill="white"
-            style={{ width: '24px', height: '24px', color: 'f1f1f1' }}
-          />
-        </button>
-      </div>
-    );
-  }
+      </button>
+    </div>
+  );
 }
 
 export default Search;
