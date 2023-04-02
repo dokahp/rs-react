3251478500f503b65d365b5f567c634361b7c7c0
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-04-02 03:01:57 +0300

    refactor: change home, about and not found components to functional components

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
-class About extends React.PureComponent {
-  render() {
-    return (
-      <div
-        style={{
-          color: '#fff',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <h3>About Project</h3>
-        <p>This project would be a Beta version of youtube! &#128513; </p>
-      </div>
-    );
-  }
+function About() {
+  return (
+    <div
+      style={{
+        color: '#fff',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h3>About Project</h3>
+      <p>This project would be a Beta version of youtube! &#128513; </p>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,17 +3,15 @@ import CardsList from '../components/CardsList/CardsList';
 import Search from '../components/Search/Search';
 import mockData from '../data/mockData';
 
-class Home extends React.PureComponent {
-  render(): React.ReactNode {
-    const { items } = mockData;
+function Home() {
+  const { items } = mockData;
 
-    return (
-      <>
-        <Search />
-        <CardsList items={items} />
-      </>
-    );
-  }
+  return (
+    <>
+      <Search />
+      <CardsList items={items} />
+    </>
+  );
 }
 
 export default Home;
