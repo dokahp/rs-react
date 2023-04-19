6d952fde67ffe35c96427ce3e22f3f69d288f83c
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-04-19 05:05:30 +0300

    fix: replace style to classname in about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
+import './styles/about.css';
 
 function About() {
   return (
-    <div
-      style={{
-        color: '#fff',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <div className="about-page">
       <h3>About Project</h3>
       <p>This project would be a Beta version of youtube! &#128513; </p>
     </div>
